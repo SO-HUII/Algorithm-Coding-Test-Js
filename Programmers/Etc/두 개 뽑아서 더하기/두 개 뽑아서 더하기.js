a18c3c49744e10038c010b96704d1828be9e50eb
sohui <sohi9203@gmail.com>
2024-04-06 23:04:50 +0900

    code: [PGS] 두 개 뽑아서 더하기 / Etc / Lv.1

---
+++ Programmers/Etc/두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+  let answer = [];
+
+  for(let i = 0; i < numbers.length; i++) {
+    for(let j = i + 1; j < numbers.length; j++) {
+      if(!answer.includes(numbers[i] + numbers[j])) answer.push(numbers[i] + numbers[j]);
+    }
+  }
+  return answer.sort((a, b) => a - b);
+}
