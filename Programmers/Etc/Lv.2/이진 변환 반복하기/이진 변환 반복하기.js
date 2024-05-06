a6cdab971164eaef6b64823340ecb3607116668b
sohui <sohi9203@gmail.com>
2024-05-06 22:37:28 +0900

    code: [PGS] 이진 변환 반복하기 / Etc/ Lv.2

---
+++ Programmers/Etc/Lv.2/이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+  let answer = [0, 0];
+  let sLen = 0;
+
+  while (s.length > 1) {
+    sLen = s.length;
+    s = s.split("0").join("");
+
+    answer[0]++;
+    answer[1] += (sLen - s.length);
+    
+    s = s.length.toString(2);
+  }
+
+  return answer;
+}
